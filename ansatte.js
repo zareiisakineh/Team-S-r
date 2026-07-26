7ac4f9bd619493bf2dcd739f43f7398ccc8779f2
zareiisakineh <zareiisakineh@gmail.com>
2026-07-26 18:49:46 +0200

    kontaktkort

---
+++ ansatte.js
@@ -0,0 +1,89 @@
+//------------------------------------------Kontaktliste for ----------------------------------
+//------------------Lager objekter for ekstravakter---------------------------------------
+
+
+
+//------------------------------------------------------------------------------------------
+//------------------------------------------Kontaktliste----------------------------------
+//------------------Lager objekter for ekstravakter---------------------------------------
+const ansatte = [
+
+{ navn:"Abdisaalam Saeed", rolle:"", telefon:"92258951", epost:"", bilde:"bilder/Abdisaalam.jpg" },
+
+{ navn:"Ibrahim Ahmednoor(SPL)", rolle:"", telefon:"99105994", epost:"", bilde:"bilder/Ibahim.jpg" },
+
+{ navn:"Albana", rolle:"", telefon:"90011223", epost:"", bilde:"bilder/Albana.jpg" },
+
+{ navn:"Amalie(SPL)", rolle:"", telefon:"91742740", epost:"", bilde:"bilder/Amalie.jpg" },
+
+{ navn:"Carlette", rolle:"", telefon:"96689375", epost:"", bilde:"bilder/Carlette.jpg" },
+
+{ navn:"Dessery(30%)", rolle:"", telefon:"99860102", epost:"", bilde:"bilder/Dessery.jpg" },
+
+{ navn:"Fekerte(SPL)", rolle:"", telefon:"97097023", epost:"", bilde:"bilder/Fekerte.jpg" },
+
+{ navn:"Gale-Ane", rolle:"", telefon:"93956855", epost:"", bilde:"bilder/Gale-Ane.jpg" },
+
+{ navn:"Gliceria(20%)", rolle:"", telefon:"92541182", epost:"", bilde:"bilder/Gliceria.jpg" },
+
+{ navn:"Glen(SPL)", rolle:"", telefon:"96669748", epost:"", bilde:"bilder/Glen.jpg" },
+
+{ navn:"Guen(SPL)", rolle:"", telefon:"45147657", epost:"", bilde:"bilder/Guen.jpg" },
+
+{ navn:"Helene", rolle:"", telefon:"41339278", epost:"", bilde:"bilder/Helene.jpg" },
+
+{ navn:"John James", rolle:"", telefon:"45586388", epost:"", bilde:"bilder/JohnJames.jpg" },
+
+{ navn:"Judelyn", rolle:"", telefon:"96656132", epost:"", bilde:"bilder/Judelyn.jpg" },
+
+{ navn:"Khirad", rolle:"", telefon:"99350304", epost:"", bilde:"bilder/Khirad.jpg" },
+
+{ navn:"Khadra(VPL)", rolle:"", telefon:"92953974", epost:"", bilde:"bilder/Khadra.jpg" },
+
+{ navn:"Khadija", rolle:"", telefon:"40077048", epost:"", bilde:"bilder/Khadija.jpg" },
+
+{ navn:"Malele(SPL)", rolle:"", telefon:"93809596", epost:"", bilde:"bilder/Malele.jpg" },
+
+{ navn:"Malyuun(SPL)", rolle:"", telefon:"40954865", epost:"", bilde:"bilder/Malyuun.jpg" },
+
+{ navn:"Marthe Sukkestad(SPL)", rolle:"", telefon:"95990136", epost:"", bilde:"bilder/MartheSukkestad.jpg" },
+
+{ navn:"Mostopha", rolle:"", telefon:"98774382", epost:"", bilde:"bilder/Mostopha.jpg" },
+
+{ navn:"Neda(SPL 20%)", rolle:" ", telefon:"90653331", epost:"", bilde:"bilder/Neda.jpg" },
+
+{ navn:"Nica", rolle:"", telefon:"93971207", epost:"", bilde:"bilder/Nica.jpg" },
+
+{ navn:"Nabina(30%)", rolle:"", telefon:"46397430", epost:"", bilde:"bilder/Nabina.jpg" },
+
+{ navn:"Preenaphan", rolle:"", telefon:"93476165", epost:"", bilde:"bilder/Preenaphan.jpg" },
+
+{ navn:"Robiel(80%)", rolle:"", telefon:"97367760", epost:"", bilde:"bilder/Robiel.jpg" },
+
+{ navn:"Silje Antonsen", rolle:"", telefon:"45179522", epost:"", bilde:"bilder/SiljeAntonsen.jpg" },
+
+{ navn:"Svetlana", rolle:"", telefon:"40571544", epost:"", bilde:"bilder/Svetlana.jpg" },
+
+{ navn:"Sevinch", rolle:"", telefon:"96685552", epost:"", bilde:"bilder/Sevinch.jpg" },
+
+{ navn:"Theresa(PB)", rolle:"", telefon:"41307331", epost:"", bilde:"bilder/Theresa.jpg" },
+
+{ navn:"Tomina", rolle:"", telefon:"48173455", epost:"", bilde:"bilder/Tomina.jpg" },
+
+{ navn:"Tuku", rolle:"", telefon:"94481168", epost:"", bilde:"bilder/Tuku.jpg" },
+
+{ navn:"Valentina", rolle:"", telefon:"91147851", epost:"", bilde:"bilder/Valentina.jpg" },
+
+{ navn:"Vilde", rolle:"", telefon:"93869136", epost:"", bilde:"bilder/Vilde.jpg" },
+
+{ navn:"Yusuf", rolle:"", telefon:"45524866", epost:"", bilde:"bilder/Yusuf.jpg" },
+
+{ navn:"Zemene(SPL)", rolle:"", telefon:"45248560", epost:"", bilde:"bilder/Zemene.jpg" },
+
+{ navn:"Zohra Dellaa", rolle:"", telefon:"45911765", epost:"", bilde:"bilder/ZohraDellaa.jpg" },
+
+{ navn:"Aase", rolle:"", telefon:"45228091", epost:"", bilde:"bilder/Aase.jpg" },
+
+{ navn:"Kristine(SPL)", rolle:"", telefon:"48626825", epost:"", bilde:"bilder/Kristine.jpg" }
+
+];
