1d432f69b232ee1f55af75ac698d811e05b6884e
zareiisakineh <zareiisakineh@gmail.com>
2026-08-05 20:59:42 +0200

    x

--- app.js
+++ app.js
@@ -2,156 +2,201 @@ import { hentAnsatte } from "./ansatteFirestore.js";
 
 console.log("app.js loaded");
 
-// Felles funksjon for alle trekkspill
-// ----------------------------------------------------------
-// toggleAccordion()
-// Denne funksjonen kjøres hver gang brukeren klikker på en
-// knapp i trekkspillet.
-//
-// HTML:
-// <button onclick="toggleAccordion(this)">
-//
-// "this" sender den knappen brukeren klikket på inn til
-// funksjonen. Parameteren "button" blir derfor en referanse
-// til akkurat denne knappen.
-// ----------------------------------------------------------
 function toggleAccordion(button) {
-    // ------------------------------------------------------
-    // nextElementSibling
-    //
-    // DOM (Document Object Model) ser HTML som et tre av elementer.
-    //
-    // HTML-en vår ser slik ut: 
-    // <button class="question">
-    // </button>
-    // <div class="answer">
-    // </div>
-    // answer-diven ligger rett etter button. nextElementSibling betyr: "Finn neste HTML-element etter denne knappen.
-    // Resultatet lagres i variabelen answer.
-    // ------------------------------------------------------
-    const answer = button.nextElementSibling;
-    // ------------------------------------------------------
-    // querySelector() 
-    // querySelector søker etter elementer ved hjelp av CSS-selectorer.
-    // ".icon" betyr: class="icon"
-    //
-    // Det viktige her er at søket skjer INNE I KNAPPEN.
-    //
-    // JavaScript leter derfor bare etter pilen som tilhører
-    // knappen brukeren klikket på.
-    // ------------------------------------------------------
-    const icon = button.querySelector(".icon");
-    // ------------------------------------------------------
-    // querySelectorAll()
-    // document betyr hele nettsiden. querySelectorAll(".answer")finner ALLE elementer som har klassen answer.
-    // Resultatet blir en NodeList (en liste over elementer).
-    // Eksempel:
-    // answer1
-    // answer2
-    // answer3
-    // answer4
-    // ------------------------------------------------------
-    document.querySelectorAll(".answer").forEach(element => {
-        // forEach går gjennom listen ett element om gangen.
-        // Første runde:
-        // item = answer1
-        // Andre runde:
-        // item = answer2
-        // osv.
-        // --------------------------------------------------
-        // !== betyr:"ikke identisk med"
-        // Vi ønsker ikke å lukke svaret brukeren nettopp klikket på. Derfor spør vi:
-        // Er dette et annet svar?
-        // Hvis ja: fjern active.
-        if (element !== answer) {
-            element.classList.remove("active");
-        }
-    });
-    // ------------------------------------------------------
-    // Nå gjør vi nøyaktig det samme med pilene.
-    // Vi finner alle ikonene på siden.
-    document.querySelectorAll(".icon").forEach(element => {
-        // Hvis ikonet ikke tilhører knappen som ble klikket,
-        // fjernes klassen open slik at pilen peker ned igjen.
-        if (element !== icon) {
-            element.classList.remove("open");
-        }
-    });
-    // classList.toggle()
-    // toggle betyr "veksle".
-    // Hvis klassen active IKKE finnes: legger JavaScript den til.
-    // Hvis klassen allerede finnes:fjerner JavaScript den.
-    //
-    // CSS bestemmer hva active betyr:
-    // .answer{
-    //     display:none;
-    // }
-    // .answer.active{
-    //     display:block;
-    // }
-    // JavaScript endrer altså bare klassene.
-    // CSS bestemmer hvordan siden skal se ut.
-    // ------------------------------------------------------
-    answer.classList.toggle("active");
-    // ------------------------------------------------------
-    // if(icon): Sjekker om variabelen icon faktisk inneholder et HTML-element.
-    // Dersom knappen av en eller annen grunn ikke har et ikon, hopper JavaScript bare over denne delen.
-    // Da unngår vi feilmeldinger.
-    // ------------------------------------------------------
-    if (icon) {
-        // Roter pilen ved å legge til eller fjerne klassen open.
-        icon.classList.toggle("open");
-    }
+
+console.log("Klikk!");
+    const item = button.parentElement;
+
+    const answer = item.querySelector(".answer");
+
+    answer.classList.toggle("open");
+
+    button.querySelector(".icon")
+        ?.classList.toggle("rotate");
 }
 // ----------------------------------------------------------
-// DOMContentLoaded:
-// Nettleseren leser HTML ovenfra og ned.
-//
-// Hvis JavaScript prøver å hente et element før HTML-en er ferdig lastet, vil elementet ikke finnes.
-// Derfor venter vi til hele DOM-en er ferdig bygd.
+document.querySelectorAll(".question")
+.forEach(button => {
+
+    button.addEventListener("click", () => {
+        toggleAccordion(button);
+    });
+
+});
+
+/* HENT ANSATTE FRA FIRESTORE */
+const ansatte = await hentAnsatte();
+// test for å se nøyaktig hvilke gruppenavn som finnes i databasen
+console.log(ansatte.map(a => a.gruppe));
+
+// Kontakt-kort
+// Lager fire lister ut fra gruppene:
+//henter alle sykepleiere fra firestore
+const sykepleier = ansatte.filter(person => {
+    return  person.gruppe === "Sykepleiere";
+});
+//henter alle Helsefagarbeidere fra ansatt.js
+const hjelpepleier = ansatte.filter(person => {
+    return  person.gruppe === "Helsefagarbeidere";
+});
+//henter alle i praktisk bistand fra ansatt.js
+const praktiskBistand = ansatte.filter(person => {
+    return  person.gruppe === "PraktiskBistand";
+});
+//henter alle ekstravakter fra ansatt.js
+const ekstravakt = ansatte.filter(person => {
+    return  person.gruppe === "Ekstravakter";
+});
+//henter alle fysioterapeuter fra ansatt.js
+const fysioterapeut = ansatte.filter(person => {
+    return  person.gruppe === "fysioterapeuter";
+});
+//henter alle fysioterapeuter fra ansatt.js
+const ergoterapeut = ansatte.filter(person => {
+    return  person.gruppe === "ergoterapeuter";
+});
+//henter alle fysioterapeuter fra ansatt.js
+const AKS = ansatte.filter(person => {
+    return  person.gruppe === "AKS";
+});
+//henter alle ernæringsfysiologer fra ansatt.js
+const ernæringsfysiolog = ansatte.filter(person => {
+    return  person.gruppe === "ernæringsfysiologer";
+});
+//henter alle ledere fra ansatt.js
+const leder = ansatte.filter(person => {
+    return  person.gruppe === "ledere";
+});
+//henter alle merkantiler fra ansatt.js
+const merkantil = ansatte.filter(person => {
+    return  person.gruppe === "merkantiler";
+});
+//henter alle nattevakter fra ansatt.js
+const nattevakt = ansatte.filter(person => {
+    return  person.gruppe === "nattevakter";
+});
+//henter alle vurderingsteam fra ansatt.js
+const vurderingsteam = ansatte.filter(person => {
+    return  person.gruppe === "vurderingsteam";
+});
+//henter alle responssenter fra ansatt.js
+const responssenter = ansatte.filter(person => {
+    return  person.gruppe === "responssenter";
+});
+
+
+ // ----------------------------
+    // Vis ansatte
+    // ----------------------------
+// Funksjon som skriver ut én gruppe
+function visGruppe(containerId, liste) {
+
+    const container = document.getElementById(containerId);
+
+    liste.forEach(person => {
+
+        container.innerHTML += `
+        
+        <div class="ansattKort">
+
+            <h3>${person.navn}</h3>
+
+            <p>${person.rolle}</p>
+
+            <div class="ikoner">
+
+                <a href="${person.bilde}"
+                   class="glightbox"
+                   title="${person.navn}">
+                    <i class="fas fa-image"></i>
+                </a>
+
+                <a href="tel:${person.telefon}">
+                    <i class="fas fa-phone"></i>
+                </a>
+
+                <a href="mailto:${person.epost}">
+                    <i class="fas fa-envelope"></i>
+                </a>
+
+            </div>
+
+        </div>
+
+        `;
+
+    });
+
+}
+
+  // ----------------------------
+    // Koble til HTML
+    // ----------------------------
+if (document.getElementById("SPL")) {
+    visGruppe("SPL", sykepleier);
+}
+
+if (document.getElementById("HPL")) {
+    visGruppe("HPL", hjelpepleier);
+}
+
+if (document.getElementById("PB")) {
+    visGruppe("PB", praktiskBistand);
+}
+
+if (document.getElementById("ekstravakt")) {
+    visGruppe("ekstravakt", ekstravakt);
+}
+
+if (document.getElementById("fysio")) {
+    visGruppe("fysio", fysioterapeut);
+}
+
+if (document.getElementById("ergo")) {
+    visGruppe("ergo", ergoterapeut);
+}
+
+if (document.getElementById("AKS")) {
+    visGruppe("AKS", AKS);
+}
+
+if (document.getElementById("ernæringFysio")) {
+    visGruppe("ernæringFysio", ernæringsfysiolog);
+}
+
+if (document.getElementById("leder")) {
+    visGruppe("leder", leder);
+}
+
+if (document.getElementById("merkantil")) {
+    visGruppe("merkantil", merkantil);
+}
+
+if (document.getElementById("nattevakt")) {
+    visGruppe("nattevakt", nattevakt);
+}
+
+if (document.getElementById("vurderingsteam")) {
+    visGruppe("vurderingsteam", vurderingsteam);
+}
+
+if (document.getElementById("responssenter")) {
+    visGruppe("responssenter", responssenter);
+}
+
 // ----------------------------------------------------------
-document.addEventListener("DOMContentLoaded", async () => {
-    // ------------------------------------------------------
-    // getElementById(): Henter ett bestemt HTML-element ved hjelp av id.
-    // HTML:
-    // <button id="menuToggle">
-    // Resultatet lagres i variabelen menuToggle.
+
     // ------------------------------------------------------
     const menuToggle = document.getElementById("menuToggle");
     const menu = document.getElementById("menu");
     // ------------------------------------------------------
-    // Denne if-setningen spør: Finnes både knappen og menyen?
-    // Hvis ett av elementene mangler, kjører ikke resten av koden.
-    // ------------------------------------------------------
     if (menuToggle && menu) {
         // --------------------------------------------------
-        // addEventListener() Lager en "lytter".
-        // JavaScript sitter nå og venter på at brukeren klikker på menyknappen.
-        // Når klikket skjer, kjøres funksjonen under.
-        // --------------------------------------------------
         menuToggle.addEventListener("click", (event) => {
-            // ----------------------------------------------
-            //Hindrer at klikket bobler videre til document   
+             
             event.stopPropagation();
-
-            // toggle("open") 
-            // Hvis open finnes: fjern den.
-            // Hvis open ikke finnes: legg den til.
-            // CSS avgjør om menyen skal være synlig.
             // ----------------------------------------------
             menu.classList.toggle("open");
-            // ----------------------------------------------
-            // setAttribute(): Endrer et attributt i HTML.
-            // aria-expanded brukes av skjermlesere.
-            // classList.contains("open"): returnerer true eller false.
-
-            // Hvis menyen er åpen:
-            // aria-expanded="true"
-            //
-            // Hvis den er lukket:
-            // aria-expanded="false"
-            //
-            // Dette gjør nettsiden mer universelt utformet.
             // ----------------------------------------------
             menuToggle.setAttribute(
                 "aria-expanded",
@@ -201,7 +246,7 @@ document.addEventListener("DOMContentLoaded", async () => {
 
     }
 
-});
+
 
 //for søkeboksen
 window.addEventListener("load", () => {
@@ -224,49 +269,6 @@ window.addEventListener("load", () => {
     }
 
 });
-
-/*const melding = document.getElementById("bursdagMelding");
-const whatsappKnapp = document.getElementById("sendWhatsapp");
-//new Date() lager et datoobjekt.Hvis datoen er 21. juli 2026, inneholder iDag: Tue Jul 21 2026
-const iDag = new Date();
-//Henter dagen. getDate() betyr Hvilken dag i måneden er det? Eksempel 21. juli gir dag = 21; i dag er object, med attributer dag og måned
-const dag = iDag.getDate();
-//Henter måneden. Månedene i js begynner med 0. Derfor adderer vi med 1
-const måned = iDag.getMonth() + 1;
-
-// Finn alle som har bursdag i dag. filter() går gjennom hele object-listen(ansatt.js) og 
-// lager en ny liste av de som har bursdag i dagens dato
-const dagensBursdager = ansatte.filter(person =>
-    person.dag === dag && person.måned === måned
-);
-const navn = dagensBursdager.map(person => person.navn); //map lager en ny liste av 
-//dagens bursdagsbarn
-
-// Har vi funnet noen?--> vis melding
-if (dagensBursdager.length > 0 && whatsappKnapp && melding) {
-    whatsappKnapp.style.display = "inline-block";
-    melding.style.display = "block"; //melding vises
-
-    melding.innerHTML = `
-            🎂🎉💕 Gratulerer med dagen kjære
-            ${navn.join(" og ")}! 🎂
-            Alle oss i Team Sør ønsker deg en riktig flott dag fylt med glede, smil og kake! !`;
-}
-
-
-/*Oppsummering av programmet
-
-Programmet følger denne rekkefølgen:
-
-📋 Lager en liste med ansatte og bursdager.
-📅 Leser dagens dato fra datamaskinen.
-🔍 Går gjennom listen med filter().
-✅ Finner alle som har bursdag i dag.
-📏 Sjekker om listen er tom med length.
-🎉 Hvis noen har bursdag, vises meldingsboksen.
-📝 map() henter ut bare navnene.
-✍️ join() setter navnene sammen til én tekst hvis flere har bursdag.
-🌐 innerHTML skriver gratulasjonsmeldingen inn på nettsiden. */
 
 //deler bursdagsmelding på whatsapp 
 const whatsappKnapp =
@@ -341,154 +343,7 @@ if (installBtn) {
 
 }
 
-// Kontakt-kort
-// Lager fire lister ut fra gruppene:
-//henter alle sykepleiere fra ansatt.js
-const sykepleier = ansatte.filter(person => {
-    return person.gruppe === "Sykepleiere";
-});
-//henter alle Helsefagarbeidere fra ansatt.js
-const hjelpepleier = ansatte.filter(person => {
-    return person.gruppe === "Helsefagarbeidere";
-});
-//henter alle i praktisk bistand fra ansatt.js
-const praktiskBistand = ansatte.filter(person => {
-    return person.gruppe === "PraktiskBistand";
-});
-//henter alle ekstravakter fra ansatt.js
-const ekstravakt = ansatte.filter(person => {
-    return person.gruppe === "Ekstravakter";
-});
-//henter alle fysioterapeuter fra ansatt.js
-const fysioterapeut = ansatte.filter(person => {
-    return person.gruppe === "fysioterapeuter";
-});
-//henter alle fysioterapeuter fra ansatt.js
-const ergoterapeut = ansatte.filter(person => {
-    return person.gruppe === "ergoterapeuter";
-});
-//henter alle fysioterapeuter fra ansatt.js
-const AKS = ansatte.filter(person => {
-    return person.gruppe === "AKS";
-});
-//henter alle ernæringsfysiologer fra ansatt.js
-const ernæringsfysiolog = ansatte.filter(person => {
-    return person.gruppe === "ernæringsfysiologer";
-});
-//henter alle ledere fra ansatt.js
-const leder = ansatte.filter(person => {
-    return person.gruppe === "ledere";
-});
-//henter alle merkantiler fra ansatt.js
-const merkantil = ansatte.filter(person => {
-    return person.gruppe === "merkantiler";
-});
-//henter alle nattevakter fra ansatt.js
-const nattevakt = ansatte.filter(person => {
-    return person.gruppe === "nattevakter";
-});
-//henter alle vurderingsteam fra ansatt.js
-const vurderingsteam = ansatte.filter(person => {
-    return person.gruppe === "vurderingsteam";
-});
-//henter alle responssenter fra ansatt.js
-const responssenter = ansatte.filter(person => {
-    return person.gruppe === "responssenter";
-});
 
 
-
-// Funksjon som skriver ut én gruppe
-function visGruppe(containerId, liste) {
-
-    const container = document.getElementById(containerId);
-
-    liste.forEach(person => {
-
-        container.innerHTML += `
-        
-        <div class="ansattKort">
-
-            <h3>${person.navn}</h3>
-
-            <p>${person.rolle}</p>
-
-            <div class="ikoner">
-
-                <a href="${person.bilde}"
-                   class="glightbox"
-                   title="${person.navn}">
-                    <i class="fas fa-image"></i>
-                </a>
-
-                <a href="tel:${person.telefon}">
-                    <i class="fas fa-phone"></i>
-                </a>
-
-                <a href="mailto:${person.epost}">
-                    <i class="fas fa-envelope"></i>
-                </a>
-
-            </div>
-
-        </div>
-
-        `;
-
-    });
-
-}
-
-if (document.getElementById("SPL")) {
-    visGruppe("SPL", sykepleier);
-}
-
-if (document.getElementById("HPL")) {
-    visGruppe("HPL", hjelpepleier);
-}
-
-if (document.getElementById("PB")) {
-    visGruppe("PB", praktiskBistand);
-}
-
-if (document.getElementById("ekstravakt")) {
-    visGruppe("ekstravakt", ekstravakt);
-}
-
-if (document.getElementById("fysio")) {
-    visGruppe("fysio", fysioterapeut);
-}
-
-if (document.getElementById("ergo")) {
-    visGruppe("ergo", ergoterapeut);
-}
-
-if (document.getElementById("AKS")) {
-    visGruppe("AKS", AKS);
-}
-
-if (document.getElementById("ernæringFysio")) {
-    visGruppe("ernæringFysio", ernæringsfysiolog);
-}
-
-if (document.getElementById("leder")) {
-    visGruppe("leder", leder);
-}
-
-if (document.getElementById("merkantil")) {
-    visGruppe("merkantil", merkantil);
-}
-
-if (document.getElementById("nattevakt")) {
-    visGruppe("nattevakt", nattevakt);
-}
-
-if (document.getElementById("vurderingsteam")) {
-    visGruppe("vurderingsteam", vurderingsteam);
-}
-
-if (document.getElementById("responssenter")) {
-    visGruppe("responssenter", responssenter);
-}
 
 
