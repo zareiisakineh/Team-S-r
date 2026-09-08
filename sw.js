92ff092a9ae0755c5e1e859a8a44e8d18a7ef416
zareiisakineh <zareiisakineh@gmail.com>
2026-09-08 22:00:05 +0200

    v199

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 // PWA CACHE + ONESIGNAL PUSH
 // ==================================================
 
-const CACHE_NAME = "team-sor-198";
+const CACHE_NAME = "team-sor-199";
 
 const FILES = [
     "./",
