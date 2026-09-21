5d025e96cf22851bf836d2151f60f1de75530438
zareiisakineh <zareiisakineh@gmail.com>
2026-09-21 12:25:09 +0200

    brev til leder

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 // PWA CACHE + ONESIGNAL PUSH
 // ==================================================
 
-const CACHE_NAME = "team-sor-221";
+const CACHE_NAME = "team-sor-222";
 
 const FILES = [
     "./",
